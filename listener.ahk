e63e2adf14d58d19b6ebc34af0a98312e3773dae
Floozutter <me@floozutter.site>
2021-08-16 16:12:51 -0700

    replace tabs with spaces

--- listener.ahk
+++ listener.ahk
@@ -4,6 +4,6 @@ global http := ComObjCreate("WinHttp.Winhttprequest.5.1")
 http.open("GET", "http://localhost:12345")
 
 F24::
-	http.send()
-	SoundPlay % "sounds\" . http.responseText
+    http.send()
+    SoundPlay % "sounds\" . http.responseText
 return
--- src/main.lua
+++ src/main.lua
@@ -2,22 +2,22 @@ local main = {}
 
 main.current_sound = nil
 main.main = function(path)
-	local sounds = dofile(path.."/sounds/sounds.lua")
-	lmc_assign_keyboard("SOUNDBODGE")
-	lmc_set_handler(
-	  "SOUNDBODGE",
-	  function(key, direction)
-		if direction ~= 0 or sounds[key] == nil then return end
-		main.current_sound = sounds[key]
-		lmc_send_keys("{F24}")
-	  end
-	)
-	lmc_http_server(
-	  12345,
-	  function(url)
-		return main.current_sound
-	  end
-	)
+    local sounds = dofile(path.."/sounds/sounds.lua")
+    lmc_assign_keyboard("SOUNDBODGE")
+    lmc_set_handler(
+        "SOUNDBODGE",
+        function(key, direction)
+            if direction ~= 0 or sounds[key] == nil then return end
+            main.current_sound = sounds[key]
+            lmc_send_keys("{F24}")
+        end
+    )
+    lmc_http_server(
+        12345,
+        function(url)
+            return main.current_sound
+        end
+    )
 end
 
 return main
